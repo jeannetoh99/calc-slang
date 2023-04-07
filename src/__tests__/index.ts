1691ec247faf02d4f6a3afa9da93400cd7ec73e7
Jeanne Toh <jeannetoh99@gmail.com>
2023-04-07 21:52:58 +0800

    Fix format

--- src/__tests__/index.ts
+++ src/__tests__/index.ts
@@ -1,34 +1,32 @@
-import {
-    expectResult,
-} from '../utils/testing'
+import { expectResult } from '../utils/testing'
 
 ////////////////////////////////////// LITERALS //////////////////////////////////////
 
 test('Empty code returns undefined', () => {
-    return expectResult('').toBe(undefined)
+  return expectResult('').toBe(undefined)
 })
 
 test('Single integer self-evaluates to itself', () => {
-    return expectResult('42;').toBe(42)
+  return expectResult('42;').toBe(42)
 })
 
 test('Single boolean self-evaluates to itself', () => {
-    return expectResult('true;').toBe(true)
+  return expectResult('true;').toBe(true)
 })
 
 test('Single real self-evaluates to itself', () => {
-    return expectResult('42.5;').toBe(42.5)
+  return expectResult('42.5;').toBe(42.5)
 })
 
 test('Single string self-evaluates to itself', () => {
-    return expectResult('"42";').toBe('42')
+  return expectResult('"42";').toBe('42')
 })
 
 test('Multiline string self-evaluates to itself', () => {
-    return expectResult(`"1\\n1";`).toBe(`1
+  return expectResult(`"1\\n1";`).toBe(`1
 1`)
 })
 
 test('Unit evaluates to undefined', () => {
-    return expectResult('();').toBe(undefined)
+  return expectResult('();').toBe(undefined)
 })
