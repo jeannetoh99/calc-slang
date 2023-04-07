749f72092ee32a5e6dea566099fea128ae100509
Jeanne Toh <jeannetoh99@gmail.com>
2023-04-08 01:48:52 +0800

    Fix format

--- src/__tests__/index.ts
+++ src/__tests__/index.ts
@@ -4,60 +4,72 @@ import { expectResult } from '../utils/testing'
 ///////////////////// LITERALS /////////////////////
 
 test('Empty code returns undefined', () => {
-  let expected : ResultType[] = []
+  const expected: ResultType[] = []
   return expectResult('').toBe(formatResults(expected))
 })
 
 test('Single integer self-evaluates to itself', () => {
-  let expected : ResultType[] = [{
-    name: "it",
-    value: 42,
-    type: "int"
-  }]
+  const expected: ResultType[] = [
+    {
+      name: 'it',
+      value: 42,
+      type: 'int'
+    }
+  ]
   return expectResult('42;').toBe(formatResults(expected))
 })
 
 test('Single boolean self-evaluates to itself', () => {
-  let expected : ResultType[] = [{
-    name: "it",
-    value: true,
-    type: "bool"
-  }]
+  const expected: ResultType[] = [
+    {
+      name: 'it',
+      value: true,
+      type: 'bool'
+    }
+  ]
   return expectResult('true;').toBe(formatResults(expected))
 })
 
 test('Single real self-evaluates to itself', () => {
-  let expected : ResultType[] = [{
-    name: "it",
-    value: 42.5,
-    type: "real"
-  }]
+  const expected: ResultType[] = [
+    {
+      name: 'it',
+      value: 42.5,
+      type: 'real'
+    }
+  ]
   return expectResult('42.5;').toBe(formatResults(expected))
 })
 
 test('Single string self-evaluates to itself', () => {
-  let expected : ResultType[] = [{
-    name: "it",
-    value:  "\"42\"",
-    type: "string"
-  }]
+  const expected: ResultType[] = [
+    {
+      name: 'it',
+      value: '"42"',
+      type: 'string'
+    }
+  ]
   return expectResult('"42";').toBe(formatResults(expected))
 })
 
 test('Multiline string self-evaluates to itself', () => {
-  let expected : ResultType[] = [{
-    name: "it",
-    value: "\"1\n1\"",
-    type: "string"
-  }]
+  const expected: ResultType[] = [
+    {
+      name: 'it',
+      value: '"1\n1"',
+      type: 'string'
+    }
+  ]
   return expectResult(`"1\\n1";`).toBe(formatResults(expected))
 })
 
 test('Unit evaluates to undefined', () => {
-  let expected : ResultType[] = [{
-    name: "it",
-    value: '()',
-    type: "unit"
-  }]
+  const expected: ResultType[] = [
+    {
+      name: 'it',
+      value: '()',
+      type: 'unit'
+    }
+  ]
   return expectResult('();').toBe(formatResults(expected))
 })
--- src/utils/stringify.ts
+++ src/utils/stringify.ts
@@ -18,19 +18,19 @@ const formatResult = (result: ResultType) => {
   return ['val', result.name, '=', result.value, ':', result.type].join(' ') + ';'
 }
 
-const extractDeclaration = (declaration: DeclarationType) : ResultType => {
+const extractDeclaration = (declaration: DeclarationType): ResultType => {
   const type = declaration.value.type === 'Literal' ? declaration.value.litType : 'function'
   const value =
     declaration.value.type === 'Literal'
       ? declaration.value.litType === 'string'
-      ? '"' + declaration.value.value + '"'
-      : declaration.value.litType === 'unit'
-      ? '()'
-      : declaration.value.litType === 'real' && isInteger(declaration.value.value)
-      ? declaration.value.value + '.0'
-      : declaration.value.value
+        ? '"' + declaration.value.value + '"'
+        : declaration.value.litType === 'unit'
+        ? '()'
+        : declaration.value.litType === 'real' && isInteger(declaration.value.value)
+        ? declaration.value.value + '.0'
+        : declaration.value.value
       : 'fn'
-  return {name: declaration.name, value, type};
+  return { name: declaration.name, value, type }
 }
 
 export const formatResults = (result: ResultType[]) => {
@@ -38,5 +38,5 @@ export const formatResults = (result: ResultType[]) => {
 }
 
 export const stringify = (value: Value): string => {
-  return value.map(extractDeclaration).map(formatResult).join("\n")
+  return value.map(extractDeclaration).map(formatResult).join('\n')
 }
